c6b6dffb0d98c8d83d155c26b90815ab0ca647fd
Adroaldo J. Pagliari <adroaldo@outlook.com>
2020-07-30 13:49:10 -0300

    feat: add celebrate joi parameters validation

--- src/modules/movements/infra/http/routes/movements.routes.ts
+++ src/modules/movements/infra/http/routes/movements.routes.ts
@@ -1,10 +1,23 @@
 import { Router } from 'express';
+import { celebrate, Segments, Joi } from 'celebrate';
 
 import MovementsController from '../controllers/MovementsController';
 
 const movementsRouter = Router();
 const movementsController = new MovementsController();
 
-movementsRouter.post('/', movementsController.index);
+movementsRouter.post(
+  '/',
+  celebrate({
+    [Segments.BODY]: {
+      current_position: Joi.string()
+        .regex(/^[A-E]{1}\d{1}$/)
+        .required(),
+      piece: Joi.string().regex(/^[pnbrqk]{1}$/),
+      color: Joi.string().regex(/^[wb]{1}$/),
+    },
+  }),
+  movementsController.index,
+);
 
 export default movementsRouter;
